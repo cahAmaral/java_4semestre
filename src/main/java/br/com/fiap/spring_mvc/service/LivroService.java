03327e8fc76c2547d3d1b64fb06d85fca0c2cc23
cahAmaral <cahxss@gmail.com>
2025-08-08 22:36:44 -0300

    aula 12/08

---
+++ src/main/java/br/com/fiap/spring_mvc/service/LivroService.java
@@ -0,0 +1,32 @@
+package br.com.fiap.spring_mvc.service;
+
+import br.com.fiap.spring_mvc.entity.Livro;
+import br.com.fiap.spring_mvc.repository.LivroRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class LivroService {
+    LivroRepository livroRepository;
+    public LivroService(LivroRepository livroRepository) {
+        this.livroRepository = livroRepository;
+    }
+    //CRUD
+
+    public Livro createLivro(Livro livro) {
+        return livroRepository.save(livro);
+
+    }
+    public Livro readLivro(Livro livro){
+        return livroRepository.findById(livro.getId()).orElse(null);
+    }
+    public List<Livro> readLivros(){
+        return livroRepository.findAll();
+
+    }
+
+    public void deleteLivro(Livro livro){
+        livroRepository.delete(livro);
+    }
+}
